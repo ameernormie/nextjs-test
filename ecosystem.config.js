7fbfc31a9571db6d3935b4bb8cdf58e8b59ec569
Ameer Hamza <ameernormie@gmail.com>
2021-09-06 22:28:15 +0500

    refactor: update deps

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,28 @@
 module.exports = {
-	apps: [
-	  {
-	    name: "NextApp",
-	    script: "npm",
-            args : "start",
-	    instances: 1,
-	    autorestart: true,
-	    watch: false,
-	    max_memory_restart: "1G",
-	  }
-	],
-	deploy: {
-	  development: {
-	    key: "~/.ssh/id_rsa",
-	    user: "webadm",
-	    host: "143.198.130.53",
-	    ref: "origin/main",
-	    ssh_options: "ForwardAgent=yes",
-	    repo: "git@github.com:ameernormie/nextjs-test.git",
-	    path: "/var/www/next-app",
-	    env: {
-	      NODE_ENV: "development",
-	    },
-	    "post-deploy":
-	      "yarn install && yarn build && yarn serve",
-	  },
-	},
-      };
-      
+  apps: [
+    {
+      name: "NextApp",
+      script: "yarn",
+      args: "start",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+    },
+  ],
+  deploy: {
+    development: {
+      key: "~/.ssh/id_rsa",
+      user: "webadm",
+      host: "143.198.130.53",
+      ref: "origin/main",
+      ssh_options: "ForwardAgent=yes",
+      repo: "git@github.com:ameernormie/nextjs-test.git",
+      path: "/var/www/next-app",
+      env: {
+        NODE_ENV: "development",
+      },
+      "post-deploy": "yarn install && yarn build && yarn serve",
+    },
+  },
+};
